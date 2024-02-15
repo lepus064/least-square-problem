308e036f6aeb39b5ef52fc01869df678a08675aa
Powei Lin <poweilin1994@gmail.com>
2024-02-15 13:17:34 -0500

    clean up

--- main2.py
+++ main2.py
@@ -8,16 +8,20 @@ np.random.seed(2)
 rvec_gt = np.random.random(3)
 rmat_gt = Rotation.from_rotvec(rvec_gt).as_matrix()
 p3d = np.random.random((3, 2))
+rotated_3d_points = rmat_gt @ p3d
 
 
-def f(rvec):
-    m = Rotation.from_rotvec(rvec).as_matrix()
-    return ((m @ p3d) - (rmat_gt @ p3d)).T.flatten()
+def cost_function(rvec: np.ndarray):
+    rotation_matrix = Rotation.from_rotvec(rvec).as_matrix()
+    return ((rotation_matrix @ p3d) - rotated_3d_points).T.flatten()
+
 
 def hat(rvec):
     return np.array([[0, -rvec[2], rvec[1]],
                      [rvec[2], 0, -rvec[0]],
                      [-rvec[1], rvec[0], 0]])
+
+
 def hat_multi(rvec: np.ndarray):
     _, dimension = rvec.reshape(3, -1).shape
     h = np.zeros((3, 3, dimension))
@@ -30,25 +34,18 @@ def hat_multi(rvec: np.ndarray):
     hts = np.dsplit(h, dimension)
     return np.vstack(hts).squeeze(axis=2)
 
+
 def rodrigues(rvec):
     rhat = hat(rvec)
     d = np.linalg.norm(rvec)
-    return np.eye(3, 3) + np.sin(d) / d * rhat + (1 - np.cos(d))/ d/ d* (rhat@rhat)
+    return np.eye(3, 3) + np.sin(d) / d * rhat + (1 - np.cos(d)) / d / d * (rhat @ rhat)
+
 
 def jacobian_l(rvec):
     rhat = hat(rvec)
     d = np.linalg.norm(rvec)
-    return np.eye(3, 3) + (1.0 - np.cos(d))/d/d*rhat + (d - np.sin(d))/d/d/d*(rhat@rhat)
+    return np.eye(3, 3) + (1.0 - np.cos(d)) / d / d * rhat + (d - np.sin(d)) / d / d / d * (rhat @ rhat)
 
-G1 = np.zeros((3, 3))
-G1[2, 1] = 1.0
-G1[1, 2] = -1.0
-G2 = np.zeros((3, 3))
-G2[2, 0] = -1.0
-G2[0, 2] = 1.0
-G3 = np.zeros((3, 3))
-G3[1, 0] = 1.0
-G3[0, 1] = -1.0
 
 if __name__ == '__main__':
 
@@ -56,35 +53,24 @@ if __name__ == '__main__':
     print("init:", rvec_noise)
 
     for iter in range(10):
-        print(iter)
-        residual = f(rvec_noise)
-        print(f"residual:{residual.shape}")
-        
-        Jx = diff_3point((residual.size, 3), f, rvec_noise)
-        print(f"Jx: {Jx.shape}\n{Jx}")
-        rmat_noise = Rotation.from_rotvec(rvec_noise).as_matrix()
-        # print("r", rmat_noise)
-        # print(rodrigues(rvec_noise))
-        # exit()
-        Rs = rmat_noise@p3d
-        j = -hat_multi(Rs) @ jacobian_l(rvec_noise)
-        # Rs1 = rmat_noise@p3d[:,1]
-        # jj1 = -hat(Rs1) @ jacobian_l(rvec_noise)
-        # j = np.vstack((jj0, jj1))
-        print(j-Jx)
-        # rr = -rmat_noise @ p3d
-        # print(rr)
+        print(f"{iter=}")
+        residual = cost_function(rvec_noise)
 
-        H = Jx.T @ Jx
-        B = -Jx.T @ residual
+        Jx_numerical = diff_3point((residual.size, 3), cost_function, rvec_noise)
+        print(f"Jx numerical: {Jx_numerical.shape}\n{Jx_numerical}")
+        rmat_noise = Rotation.from_rotvec(rvec_noise).as_matrix()
+
+        Rs = rmat_noise @ p3d
+        Jx_analytic = -hat_multi(Rs) @ jacobian_l(rvec_noise)
+        print(f"Jx analytic: {Jx_analytic.shape}\n{Jx_analytic}")
+
+        H = Jx_numerical.T @ Jx_numerical
+        B = -Jx_numerical.T @ residual
         dx = scipy.linalg.solve(H, B)
-        if(np.linalg.norm(dx) < 1e-8):
+        if (np.linalg.norm(dx) < 1e-8):
             break
         rvec_noise += dx
         print()
     print("end")
-    print(rvec_gt)
-    print(rvec_noise)
-
-# print("rmat gt:\n", rmat_gt)
-# print(j)
+    print(f"ground truth:     {rvec_gt}")
+    print(f"optimized result: {rvec_noise}")
